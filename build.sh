51c603b08ed0c902cb4785bd2a5addff47cd3df3
Allister Beharry <allisterbeharry@yahoo.com>
2019-02-05 21:26:43 +0000

    Updated build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -e
 rm -fr Analyzers/*
 rm -fr Rules/*
 rm -fr Examples/*
-TERM=xterm mono ./nuget/nuget.exe restore DevAudit.Mono.sln && mono xbuild DevAudit.Mono.sln /p:Configuration=RuntimeDebug $*
+TERM=xterm mono .nuget/nuget.exe restore DevAudit.Mono.sln && mono xbuild DevAudit.Mono.sln /p:Configuration=RuntimeDebug $*
